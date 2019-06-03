750372cd656f357d573a731af47ccba6fbad62d6
Luc Klaassen <luc.klaassen@infosupport.com>
2019-06-03 20:25:14 +0200

    remove test file

--- run_test.ps1
+++
@@ -1,7 +0,0 @@
-rm -r C:\work\maven\apache-maven\target\apache-maven-3.6.2-SNAPSHOT
-
-mvn -DskipTests=true package
-
-tar -xzvf C:\work\maven\apache-maven\target\apache-maven-3.6.2-SNAPSHOT-bin.tar.gz -C C:\work\maven\apache-maven\target\
-
-C:\work\maven\apache-maven\target\apache-maven-3.6.2-SNAPSHOT\bin\mvn.cmd -f C:\Users\LucK\IdeaProjects\slf4jadapter package -e --fail-level WARN
